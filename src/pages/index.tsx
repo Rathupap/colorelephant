eda19259e0ad3cdb6d91a089fc7023f70d2ca28b
Prince Rathupa <princer@reactgeeks.com>
2021-11-24 20:56:36 +0200

    Removed any type for response data results

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,23 @@ import { Movie } from "types";
 import MovieItem from "components/MovieItem";
 import { orderBy } from "lodash";
 
+interface Result {
+  adult?: boolean,
+  backdrop_path?: string | null,
+  genre_ids?: number[],
+  id?: number,
+  original_language?: string,
+  original_title?: string,
+  overview?: string,
+  popularity?: number,
+  poster_path?: string | null,
+  release_date?: string,
+  title?: string,
+  video?: boolean,
+  vote_average?: number,
+  vote_count?: number
+}
+
 const sortBy = ["Year Desc", "Year Asc", "Title Asc", "Title Desc", "Rating Desc", "Rating Asc"];
 
 const Home = ({ movies }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
@@ -101,12 +118,13 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
 
   try {
     const data = await (await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_KEY}`)).json();
-    const moviesData: Movie[] = data.results.map((result: any) => ({
+    console.log(data.results[0].genre_ids)
+    const moviesData: Movie[] = data.results.map((result: Result) => ({
       id: result.id,
       title: result.title,
       image: `https://image.tmdb.org/t/p/w200${result.poster_path}`,
       rating: result.vote_average,
-      year: new Date(result.release_date).getFullYear(),
+      year: new Date(result.release_date as string).getFullYear(),
       liked: false
     }));
     return {
